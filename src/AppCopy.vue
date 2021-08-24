48b3c3c90bffc40d4e1d20b2ec3c974068bb1d97
Matiiv-vb <vasya.matiyiv@gmail.com>
2021-08-24 20:33:20 +0300

    App copy

---
+++ src/AppCopy.vue
@@ -0,0 +1,189 @@
+<template>
+  <div id="app">
+   <div id="components-demo">
+     <button-counter
+     
+      v-for="(feald, i) in info"
+      v-bind:name="feald.name"
+      v-model='feald.value'
+      ></button-counter>
+   </div>
+
+  </div>
+
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      info: [
+					{
+						name: 'Name',
+						value: '',
+						pattern: /^[a-zA-Z ]{2,30}$/
+					},
+					{
+						name: 'Phone',
+						value: 'dd',
+						pattern: /^[0-9]{7,14}$/
+					},
+					{
+						name: 'Email',
+						value: '',
+						pattern: /.+/
+					},
+					{
+						name: 'Some Field 1',
+						value: '',
+						pattern: /.+/
+					},
+					{
+						name: 'Some Field 2',
+						value: '',
+						pattern: /.+/
+					}
+				],
+      
+
+
+    }
+  },
+  methods: {
+
+
+  },
+  computed: {
+
+   
+   
+  },
+  beforeCreate() {
+    console.log(app, 'beforeCreate');
+    
+    },
+
+  created() {
+    console.log(app, 'created');
+    console.log(this.info.forEach((e)=>{e.valid=false; e.activated=false}), 'Create inf');
+    },
+
+  beforeMount() {
+    console.log(app, 'beforeMount');
+    },
+
+  mounted() {
+    // this.$nextTick(()=>{
+    //   this.$refs.fInput.focus()
+    // })
+    console.log(app, 'mounted');
+    
+    },
+
+  beforeUpdate() {
+    console.log(app, 'beforeUpdate');
+  },
+
+  updated() {
+    console.log(app, 'updated');
+  },
+  beforeDestroy() {
+    console.log(app, 'beforeDestroy');
+  },
+  destroyed() {
+    console.log(app, 'destroyed');
+  }
+}
+
+
+</script>
+
+<style lang='scss'>
+
+
+.fzBig {
+  font-size: 40px;
+}
+
+
+.content{
+			height: 300px;
+			overflow: auto;
+		}
+
+.red {
+  background-color: rgb(109, 62, 69);
+  color: red;
+}
+
+.orange {
+  background-color: rgb(175, 72, 12);
+  color: orange
+}
+
+.blue {
+  background-color: rgb(8, 118, 122);
+  color: blue;
+}
+
+input {
+  margin: 5px 20px;
+}
+
+.my-button {
+  display: block;
+  position: relative;
+  left: 5px;
+  margin: 0px 20px;
+}
+
+.form-group {
+  text-align: left;
+  label {
+    margin: 0px 20px;
+  }
+}
+.wrapper  {
+  width: 700px;
+}
+
+
+.jj {
+  height: 300px;
+}
+
+.btn-danger {
+  color: yellow;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import Vue from "vue";
-import App from "./App.vue";
+import App from "./AppCopy.vue";
+// import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
+
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  props: ['name', "value"],
+  template: `<div>
+            <div class="form-group"   >
+              <label >{{name}} {{value}}</label>
+              <input type="text" class="form-control" 
+               v-bind:value="value"
+               v-on:input="$emit('input', $event.target.value)" >
+            </div>
+          </div>`
+})
 
 new Vue({
   el: "#app",
